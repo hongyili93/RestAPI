cb3c83b8fb67e1fb41bd3f278815f1463bed5f8e
hongyi <hongyili93@gmail.com>
2018-07-05 17:31:12 -0700

    change the auth method from session to token

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -1,30 +1,52 @@
 const express = require('express');
 const Todo = require('../models/todoModel');
 const todoRouter = express.Router();
-todoRouter.route('/')
-    .get((req, res) => {
-        Todo.find({'UserId':req.session.userId}, (err, todos) => {
-            //console.log(todos);
-            res.json(todos)
+var userModel = require('../models/userModel');
+var bodyParser = require('body-parser');
+
+var verifyToken = require('./VerifyToken');
+
+todoRouter.use(bodyParser.urlencoded({ extended: false }));
+todoRouter.use(bodyParser.json());
+
+todoRouter.get('/', verifyToken, function (req, res, next) {
+    userModel.findById(req.userId, { password: 0 }, function (err, user) {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+        Todo.find({'UserId':req.userId}, (err, todos) => {
+            res.status(200).send(todos);
         })
-    })
-    .post((req, res) => {
+    });
+});
+
+todoRouter.post('/', verifyToken, function (req, res, next) {
+    userModel.findById(req.userId, { password: 0 }, function (err, user) {
+
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
         let todo = new Todo(req.body);
-        todo.UserId = req.session.userId;
+        todo.UserId = req.userId;
         todo.save();
         res.status(201).send(todo)
-    })
+    });
+});
+
 
 // Middleware
-todoRouter.use('/:todoId', (req, res, next)=>{
-    Todo.findById( req.params.todoId, (err,todo)=>{
-        if(err)
-            res.status(500).send(err)
-        else {
-            req.todo = todo;
-            next()
-        }
-    })
+todoRouter.use('/:todoId', verifyToken, (req, res, next)=>{
+    userModel.findById(req.userId, { password: 0 }, function (err, user) {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+        Todo.findOne({ '_id':req.params.todoId, 'UserId':req.userId }, (err,todo)=>{
+            if(err)
+                res.status(500).send(err)
+            else {
+                if (!todo) return res.status(404).send("No todo found.");
+                req.todo = todo;
+                next()
+            }
+        })
+    });
 })
 todoRouter.route('/:todoId')
     .get((req, res) => {
@@ -42,6 +64,9 @@ todoRouter.route('/:todoId')
         if(req.body._id){
             delete req.body._id;
         }
+        if(req.body.UserId){
+            delete req.body._id;
+        }
         for( let p in req.body ){
             req.todo[p] = req.body[p]
         }
@@ -49,14 +74,14 @@ todoRouter.route('/:todoId')
         res.json(req.todo)
     })
     .delete((req,res)=>{
-        req.todo.remove(err => {
-            if(err){
-                res.status(500).send(err)
-            }
-            else{
-                res.status(204).send('removed')
-            }
-        })
+        Todo.findByIdAndRemove(req.params.todoId, (err, todo) => {
+            if (err) return res.status(500).send(err);
+            const response = {
+                message: "Todo successfully deleted",
+                id: todo._id
+            };
+            return res.status(200).send(response);
+        });
     })
 
 module.exports = todoRouter;
